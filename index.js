6344e6f2c6c1b29a5c48183ddbefe9d4843ec23a
nurajalali <jalalinura@gmail.com>
2023-05-21 16:32:55 +0330

    notif project master

---
+++ index.js
@@ -0,0 +1,20 @@
+let read = 7;
+document.getElementById("reads").innerText = read;
+
+const elements = Array.from(document.getElementsByClassName("notif"));
+
+elements.forEach((e) =>
+  e.addEventListener(
+    "click",
+    (event) => {
+      read -= 1;
+      document.getElementById("reads").innerText = read;
+      event.target.classList.add("read");
+    },
+    { once: true }
+  )
+);
+
+document.getElementById("read-all").addEventListener("click", () => {
+  elements.forEach((e) => e.dispatchEvent(new Event("click")));
+});
